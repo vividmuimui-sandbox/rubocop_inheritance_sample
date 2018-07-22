bf72d46f8a9fce18026599a152e5af09887d42d5
vividmuimui <tkoyama@aiming-inc.com>
2018-07-22 23:32:27 +0900

    bundle gem dummy_gem

---
+++ dummy_gem/spec/dummy_gem_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe DummyGem do
+  it "has a version number" do
+    expect(DummyGem::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
